0daffa879b0fcd984fdf3567a0433010af7130f3
Mmatuka0910 <mothebemmatuka@gmail.com>
2023-06-18 16:52:29 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,6 +12,7 @@ int main(void)
 	int d;
 
 	c = 'a';
+	d = 0;
 	while
 		(d < 10) {
 			putchar(d + '0');
@@ -23,4 +24,5 @@ int main(void)
 			c++;
 		}
 	putchar('\n');		
-	return (0);	
+	return (0);
+}	
